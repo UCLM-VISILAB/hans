109e13bacefd7fe168d25735ff1134cd201e5d53
Pablo del Hoyo Abad <pablo.hoyo@alu.uclm.es>
2024-05-13 19:16:54 +0200

    Bypass 1000 limit when list S3 objects using a paginator

--- backend/src/context/__init__.py
+++ backend/src/context/__init__.py
@@ -7,6 +7,7 @@ from .session import Session
 COLLECTION_FOLDER = Path('questions')
 SESSION_LOG_FOLDER = Path('session_log')
 
+
 class AppContext:
     args = Namespace(
         mqtt_port=9001,
@@ -21,24 +22,25 @@ class AppContext:
     # Configura el cliente de S3
     s3 = boto3.client('s3')
     bucket_name = 'hans-platform-collections'
+
     @staticmethod
     def reload_collections():
-        # Listar objetos en el bucket
-        response = AppContext.s3.list_objects_v2(Bucket=AppContext.bucket_name)
+        paginator = AppContext.s3.get_paginator("list_objects_v2")
+        pages = paginator.paginate(Bucket=AppContext.bucket_name)
 
         # Procesar objetos y construir el diccionario
-        for obj in response.get('Contents', []):
-            collection_name = obj['Key'].split('/')[0]
-            file_name = obj['Key'].split('/')[1]
-            
-            # Verificar si el nombre del objeto comienza con "Question"
-            if file_name.startswith('Question'):
-                # Obtén el conjunto de objetos para esta colección, o crea uno nuevo si no existe
-                questions = AppContext.collections.get(collection_name, set())
-                
-                # Agrega el nombre del objeto al conjunto
-                questions.add(file_name)
-                
-                # Actualiza el diccionario con el conjunto actualizado
-                AppContext.collections[collection_name] = questions
+        for page in pages:
+            for obj in page.get('Contents', []):
+                collection_name = obj['Key'].split('/')[0]
+                file_name = obj['Key'].split('/')[1]
 
+                # Verificar si el nombre del objeto comienza con "Question"
+                if file_name.startswith('Question'):
+                    # Obtén el conjunto de objetos para esta colección, o crea uno nuevo si no existe
+                    questions = AppContext.collections.get(collection_name, set())
+
+                    # Agrega el nombre del objeto al conjunto
+                    questions.add(file_name)
+
+                    # Actualiza el diccionario con el conjunto actualizado
+                    AppContext.collections[collection_name] = questions
